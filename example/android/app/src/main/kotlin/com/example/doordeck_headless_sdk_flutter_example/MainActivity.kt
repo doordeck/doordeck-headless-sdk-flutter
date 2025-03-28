62e147505bba409046115cd5188128b78241b669
Rafa Ruiz <raruizmu@gmail.com>
2025-03-28 12:50:03 +0100

    Clean up main branch

--- example/android/app/src/main/kotlin/com/example/doordeck_headless_sdk_flutter_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.doordeck.doordeck_headless_sdk_flutter_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- example/lib/styles/styles.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-InputDecoration inputDecoration(String label) => InputDecoration(
-      labelText: label,
-      border: const OutlineInputBorder(),
-    );
-
-const TextStyle errorTextStyle = TextStyle(color: Colors.red);
